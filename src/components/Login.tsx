22648e372c12d13eac5304fdce188acdc5bee6f4
Francis <gan_fu_rui@hotmail.com>
2021-01-05 15:23:43 +0900

    added the login layout

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,7 +6,13 @@ function Login() {
   return (
     <div className="Login">
         <div className="title">Peekify</div>
-
+        <label >User Name<br/>
+        <input type="text" id='username'></input>
+        </label><br />
+        <label >Password<br/> 
+        <input type="text" id='password'></input>
+        </label>
+        
     </div>
   );
 }
