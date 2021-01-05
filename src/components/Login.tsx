22370ddfaaea7fd2c23b9e0800b0f3444f8a9906
Francis <gan_fu_rui@hotmail.com>
2021-01-05 15:12:18 +0900

    did some components and styled the page slightly

---
+++ src/components/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './login.css';
+
+function Login() {
+  return (
+    <div className="Login">
+        <div className="title">Peekify</div>
+
+    </div>
+  );
+}
+
+export default Login;
