6688d14ff9a2340ed071520f9c6a7e7d5941ce65
Francis <gan_fu_rui@hotmail.com>
2021-01-06 14:33:42 +0900

    improved the register rooter

--- client/src/components/Register/index.tsx
+++ client/src/components/Register/index.tsx
@@ -20,31 +20,32 @@ export default function Register() {
 	}
 
 	return (
-		<div className='register'>
-		<div className="form">
-			<h1>Register</h1>
-			<form className="register-fields">
-				<TextField
-					fullWidth
-					placeholder="you@email.com"
-					label="Your Email"
-					value={email}
-					onChange={(e: any) => setEmail(e.target.value)}
-					variant="outlined"
-				/>
-				<TextField
-					fullWidth
-					value={password}
-					onChange={(e: any) => setPassword(e.target.value)}
-					placeholder="p@$$w0rd"
-					label="Password"
-					variant="outlined"
-				/>
-				<Button color="primary" variant="contained" onClick={registerUser}>
+
+		<div className="Login">
+      
+        <div className="title">Peekify</div>
+            <label className='password'>Email<br/>
+            <input type="text"
+								className='email-field'
+								placeholder="you@email.com"
+								value={email}
+								onChange={(e: any) => setEmail(e.target.value)}></input>
+            </label>
+            <label className='password'>Password<br/> 
+            <input type="text" 
+								className='password-field'
+								value={password}
+								onChange={(e: any) => setPassword(e.target.value)}
+								placeholder="p@$$w0rd"></input>
+            </label>
+            <br />
+			<Button  variant="contained" onClick={registerUser}>
 					Register
 				</Button>
-			</form>
-		</div>
+        
+
+
+
 		</div>
 	);
 }
