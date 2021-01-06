73c57ee69bb8782deaf4b80f644c1e153f9b8762
Francis <gan_fu_rui@hotmail.com>
2021-01-06 12:25:46 +0900

    changed the style of the Home page

--- client/src/components/Home/index.tsx
+++ client/src/components/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
+
 export default function Home() {
 	return (
 		<div className="App">
