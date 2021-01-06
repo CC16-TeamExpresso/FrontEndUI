b17415732f7950107f34ab14a550c33e94d9372b
Nada <olayan.nada@nagoya-u.jp>
2021-01-06 15:36:50 +0900

    style merged and profile page content deleted

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -2,45 +2,9 @@ import React, { useState } from 'react';
 import { TextField, Button } from '@material-ui/core';
 
 export default function Register() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-
-	async function loginUser() {
-		const res = await fetch('http://localhost:8050/api/login', {
-			method: 'POST',
-			headers: { 'content-type': 'application/json' },
-			body: JSON.stringify({
-				email,
-				password,
-			}),
-		}).then((t) => t.json());
-		console.log(res);
-	}
-
 	return (
 		<div>
-			<h1>Login</h1>
-			<form className="register-fields">
-				<TextField
-					fullWidth
-					placeholder="you@email.com"
-					label="Your Email"
-					value={email}
-					onChange={(e: any) => setEmail(e.target.value)}
-					variant="outlined"
-				/>
-				<TextField
-					fullWidth
-					value={password}
-					onChange={(e: any) => setPassword(e.target.value)}
-					placeholder="p@$$w0rd"
-					label="Password"
-					variant="outlined"
-				/>
-				<Button color="primary" variant="contained" onClick={loginUser}>
-					login
-				</Button>
-			</form>
+			<h1>Profile</h1>
 		</div>
 	);
 }
